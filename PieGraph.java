0ebddbb8dd77e5092938e90403fca018737b721f
altan4377 <45436316+altan4377@users.noreply.github.com>
2019-01-08 14:28:59 -0500

    Add files via upload

---
+++ PieGraph.java
@@ -0,0 +1,135 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+
+package finalproject.tang.chen;
+
+import java.text.AttributedString;
+import javax.swing.JPanel;
+import org.jfree.chart.ChartFactory;
+import org.jfree.chart.ChartPanel;
+import org.jfree.chart.JFreeChart;
+import org.jfree.chart.labels.PieSectionLabelGenerator;
+import org.jfree.chart.plot.PiePlot3D;
+import org.jfree.data.general.DefaultPieDataset;
+import org.jfree.data.general.PieDataset;
+import org.jfree.ui.ApplicationFrame;
+import org.jfree.util.Rotation;
+/**
+ * A pie chart with a custom label generator.
+ */
+public class PieGraph extends ApplicationFrame {
+
+    /**
+     * Creates a new demo.
+     *
+     * @param title  the frame title.
+     */
+    public PieGraph(String title) {
+
+        super(title);
+        PieDataset dataset = createDataset();
+        JFreeChart chart = createChart(dataset);
+        ChartPanel chartPanel = new ChartPanel(chart);
+        chartPanel.setPreferredSize(new java.awt.Dimension(500, 270));
+        setContentPane(chartPanel);
+
+    }
+
+    /**
+     * Creates a sample dataset for the demo.
+     *
+     * @return A sample dataset.
+     */
+    private static PieDataset createDataset() {
+
+        DefaultPieDataset result = new DefaultPieDataset();
+        result.setValue("Java", new Double(43.2));
+        result.setValue("Visual Basic", new Double(10.0));
+        result.setValue("C/C++", new Double(17.5));
+        result.setValue("PHP", new Double(32.5));
+        result.setValue("Perl", new Double(1.0));
+        return result;
+
+    }
+
+    /**
+     * Creates a sample chart.
+     *
+     * @param dataset  the dataset.
+     *
+     * @return A chart.
+     */
+    private static JFreeChart createChart(PieDataset dataset) {
+
+        JFreeChart chart = ChartFactory.createPieChart3D(
+            "Pie Chart 3D Demo 3",  // chart title
+            dataset,                // data
+            true,                   // include legend
+            true,
+            false
+        );
+
+        PiePlot3D plot = (PiePlot3D) chart.getPlot();
+        plot.setStartAngle(290);
+        plot.setDirection(Rotation.CLOCKWISE);
+        plot.setForegroundAlpha(0.5f);
+        plot.setNoDataMessage("No data to display");
+        plot.setLabelGenerator(new CustomLabelGenerator());
+        return chart;
+
+    }
+
+    /**
+     * Creates a panel for the demo (used by SuperDemo.java).
+     *
+     * @return A panel.
+     */
+    public static JPanel createDemoPanel() {
+        JFreeChart chart = createChart(createDataset());
+        return new ChartPanel(chart);
+    }
+
+
+    /**
+     * A custom label generator (returns null for one item as a test).
+     */
+    static class CustomLabelGenerator implements PieSectionLabelGenerator {
+
+        /**
+         * Generates a label for a pie section.
+         *
+         * @param dataset  the dataset (<code>null</code> not permitted).
+         * @param key  the section key (<code>null</code> not permitted).
+         *
+         * @return the label (possibly <code>null</code>).
+         */
+        public String generateSectionLabel(PieDataset dataset, Comparable key) {
+            String result = null;
+            if (dataset != null) {
+                if (!key.equals("PHP")) {
+                    result = key.toString();
+                }
+            }
+            return result;
+        }
+
+        /**
+         * Not used.
+         *
+         * @param dataset  the dataset.
+         * @param key  the key.
+         *
+         * @return null.
+         */
+        public AttributedString generateAttributedSectionLabel(
+                PieDataset dataset, Comparable key) {
+            return null;
+        }
+
+    }
+
+}
---
+++ SingleMC.java
@@ -0,0 +1,165 @@
+/*
+ * SingleMC class
+ */
+package finalproject.tang.chen;
+
+import java.util.ArrayList;
+
+public class SingleMC extends Question {
+
+    private String question;
+    private int userSelected;
+    private ArrayList<String> answers;
+    private int[] collectionOfAnswers = new int[12];
+
+    /**
+     * This is the primary constructor of the SingleMC class
+     */
+    public SingleMC() {
+        question = "";
+        userSelected = 0;
+        answers = new ArrayList();
+        for(int i = 0; i < 12; i++){
+            collectionOfAnswers[i] = 0;
+        }
+    }
+
+    /**
+     * This is the secondary constructor of the SingleMC class
+     *
+     * @param question - the question of this multiple choice question
+     * @param answers - the list of answers available to be selected by the user
+     */
+    public SingleMC(String question, ArrayList<String> answers) {
+        this();
+        this.question = question;
+        this.answers = answers;
+        for(int i = 0; i < 12; i++){
+            collectionOfAnswers[i] = 0;
+        }
+    }
+
+    /**
+     * This is the tertiary constructor of the SingleMC class
+     *
+     * @param question - the question of this multiple choice question
+     * @param answers - the list of answers available to be selected by the user
+     * @param userSelected - the index of the choice of the user in the list
+     */
+    public SingleMC(String question, ArrayList<String> answers, int userSelected) {
+        this(question, answers);
+        this.userSelected = userSelected;
+        addAnswerToCollection(collectionOfAnswers, userSelected);
+    }
+
+    
+    public SingleMC(String question, ArrayList<String> answers, int userSelected, int[] collectionOfAnswers) {
+        this(question, answers, userSelected);
+        this.collectionOfAnswers = collectionOfAnswers;
+    }
+
+    /**
+     * This method will add the selected answer to an array of responses
+     *
+     * @param collectionOfAnswers - the array of answers storing the counter of
+     * selected answers
+     * @param userSelected - the index of the user's selected answer
+     * @return - the updated answer counter storing array
+     */
+    public int[] addAnswerToCollection(int[] collectionOfAnswers, int userSelected) {
+        collectionOfAnswers[userSelected] += 1;
+        return collectionOfAnswers;
+    }
+
+    /**
+     * This method is the equals method of the SingleMC class
+     *
+     * @param comparedQuestion - the SingleMC to be compared to
+     * @return - true or false if the string and answers contained in the
+     * SingleMC is the same
+     */
+    public boolean equals(SingleMC comparedQuestion) {
+        return this.question.equals(comparedQuestion.question)
+                && this.answers.containsAll(comparedQuestion.answers)
+                && comparedQuestion.answers.containsAll(this.answers);
+    }
+
+    /**
+     * This is the clone method for a SingleMC question
+     *
+     * @return a new SingleMC question with the same question and answer
+     * selections
+     */
+    public SingleMC clone() {
+        return new SingleMC(question, answers);
+    }
+
+    /**
+     * This is the toString method of the SingleMC class
+     *
+     * @return the formatted representation of the SingleMC
+     */
+    public String toString() {
+        String out = "";
+        out += "Question: " + question + "\n";
+        out += "Answers: " + printList(answers) + "\n";
+        return out;
+    }
+
+    /**
+     * This method will print out all the elements in a list
+     *
+     * @param answers - the array list of selectable choices of a single MC
+     * question
+     * @return - string of all the elements in the list
+     */
+    public String printList(ArrayList<String> answers) {
+        String out = "";
+        for (String str : answers) {
+            out += str + "\n";
+        }
+        return out;
+    }
+
+    /**
+     * @return the question
+     */
+    public String getQuestion() {
+        return question;
+    }
+
+    /**
+     * @param question the question to set
+     */
+    public void setQuestion(String question) {
+        this.question = question;
+    }
+
+    /**
+     * @return the userSelected
+     */
+    public int getUserSelected() {
+        return userSelected;
+    }
+
+    /**
+     * @param userSelected the userSelected to set
+     */
+    public void setUserSelected(int userSelected) {
+        this.userSelected = userSelected;
+    }
+
+    /**
+     * @return the answers
+     */
+    public ArrayList<String> getAnswers() {
+        return answers;
+    }
+
+    /**
+     * @param answers the answers to set
+     */
+    public void setAnswers(ArrayList<String> answers) {
+        this.answers = answers;
+    }
+}
